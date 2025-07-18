1c1d630f616ebde6580b3b423fceeb7bde7c556f
Ernesto Maria <ernestomaria93@gmail.com>
2025-07-18 06:54:07 -0500

    db settings

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,12 @@
-import "dotenv/config";
 import express from "express";
+import { ENV } from "./config/env.js";
 
 const app = express();
-const PORT = process.env.PORT || 8001;
+const PORT = ENV.PORT || 5001;
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ success: true, message: "a pipocar" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
